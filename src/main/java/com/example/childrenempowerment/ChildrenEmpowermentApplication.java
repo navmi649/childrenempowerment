7e4ab03ac62cf08572a52243c131015e88db9582
navmi649 <navamijith05@gmail.com>
2026-03-28 21:52:41 +0530

    Added basic project structure and files

---
+++ src/main/java/com/example/childrenempowerment/ChildrenEmpowermentApplication.java
@@ -0,0 +1,13 @@
+package com.example.childrenempowerment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenEmpowermentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildrenEmpowermentApplication.class, args);
+	}
+
+}
